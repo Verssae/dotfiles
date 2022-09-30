861ca56c49d77c19aa6d0e5b4e6f854cf348a09b
Hansae Ju <sparky@hanyang.ac.kr>
2022-09-30 15:55:50 +0900

    MOD: rm some key maps

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,14 +1,7 @@
-local keymap = vim.keymap
-
--- New tab
-keymap.set('n', 'te', ':tabedit')
-
 -- Format Code
-keymap.set('n', '<C-f>', ':Neoformat<CR>')
-
--- Undo
-keymap.set('n', '<C-z>', ':undo<CR>')
+vim.keymap.set('n', '<C-f>', ':Neoformat<CR>')
 
 -- Tabs + Bufferline
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', {})
+-- vim.keymap.set(vn', '<C-w>', ':bdelete<CR>')
