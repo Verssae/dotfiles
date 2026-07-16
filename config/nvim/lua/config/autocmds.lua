2076c7c8294eab3014bab0ef47e0a6b5bd69a260
Hansae Ju <sparky@hanyang.ac.kr>
2026-07-16 09:54:25 +0900

    nvim: markdown/text에서 LazyVim이 켜는 wrap 비활성화

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- LazyVim의 lazyvim_wrap_spell이 markdown/text에서 wrap을 켜므로 다시 끈다 (spell은 유지)
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("no_wrap", { clear = true }),
+  pattern = { "markdown", "text", "gitcommit" },
+  callback = function()
+    vim.opt_local.wrap = false
+  end,
+})
